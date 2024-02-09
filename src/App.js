6f61ab658c398b00f98906b002ad06428f285695
Gaurav Lokhande <lokhandeg570@gmial.com>
2024-02-09 21:56:34 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
+import { Link, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import About from './component/About';
+import Contact from './component/Contact';
+import Home from './component/Home';
+import Profile from './component/Profile';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />
+    },
+    {
+      path: "/about",
+      element: <About />
+    },
+    {
+      path: "/contact",
+      element: <Contact />
+    },
+    {
+      path: "/profile/:id",
+      element: <Profile />
+    },
+  ]);
+
+  const handleSelectChange = (event) => {
+    const selectedPath = event.target.value;
+    router.navigate(selectedPath);
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <select onChange={handleSelectChange}>
+          <option value="/">Home</option>
+          <option value="/about">About</option>
+          <option value="/contact">Contact</option>
+          <option value="/profile/2">Profile</option>
+        </select>
+      </div>
+      <hr></hr>
+      <div><RouterProvider router={router} /></div>
     </div>
   );
 }
